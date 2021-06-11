7adcb56643d6354b401369fc66d5e31a2da006b0
3aly <Thebigali1998@gmail.com>
2021-06-11 15:43:22 +0200

    added LED function that turns on when distance > 100

---
+++ GPS/led.c
@@ -0,0 +1,22 @@
+#include "C:/Keil/Labware/TM4C123GH6PM.h"
+#include "led.h"
+
+void InitPortF(void)
+{
+	SYSCTL_RCGC2_R |= 0x20 ;  //enable Port F
+	while((SYSCTL_PRGPIO_R&0x00000020)==0){}  //Wait until Port F is ready
+	GPIO_PORTF_LOCK_R	= 0x4C4F434B ;	
+	GPIO_PORTF_DIR_R = 0x0E ;  //input PF0 , PF4 ,, output PF1-3
+	GPIO_PORTF_CR_R = 0x1F ;  //enable changes for PF0-PF4
+	GPIO_PORTF_DEN_R = 0x1F ;  //digital PF0-4
+	GPIO_PORTF_AFSEL_R = 0 ;  //Clear PF0-4
+	GPIO_PORTF_PUR_R = 0x11 ;  //pull up resistor for PF0 , PF4
+	GPIO_PORTF_AMSEL_R = 0 ;  //Clear PF0-4
+	GPIO_PORTF_PCTL_R = 0 ;
+	
+}
+
+void Output(uint32_t data)
+{
+	GPIO_PORTF_DATA_R = data ;
+}
---
+++ GPS/led.h
@@ -0,0 +1,8 @@
+#ifndef LED_H_
+#define LED_H_
+#include "stdint.h"
+
+void InitPortF(void);
+void Output(uint32_t data);
+
+#endif
